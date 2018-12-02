8c6fc43a8a6bd52ba543f7a33bb8acb07ab7933f
Dmitry Banasko <dimon.bad@gmail.com>
2018-12-02 15:49:46 -0500

    feat: gunner simple fire but only once

--- SacrificeGame/war/soldier_gun.go
+++ SacrificeGame/war/soldier_gun.go
@@ -1,10 +1,43 @@
+components {
+  id: "soldier1"
+  component: "/war/soldier.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
-  id: "sprite"
+  id: "soldier"
   type: "sprite"
   data: "tile_set: \"/assets/atlases/soldier.atlas\"\n"
   "default_animation: \"idle_gun\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bulletfactory"
+  type: "factory"
+  data: "prototype: \"/war/bullet_gunner.go\"\n"
+  "load_dynamically: false\n"
   ""
   position {
     x: 0.0
