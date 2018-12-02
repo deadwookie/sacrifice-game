334435d67e049a1676227fe3506d04d5b5cde5a5
Dmitry Banasko <dimon.bad@gmail.com>
2018-12-02 17:55:58 -0500

    feat: try to animate gunner shot

--- SacrificeGame/war/soldier_gun.go
+++ SacrificeGame/war/soldier_gun.go
@@ -51,3 +51,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "fire"
+  type: "sprite"
+  data: "tile_set: \"/assets/atlases/fire_gunner.atlas\"\n"
+  "default_animation: \"fire\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: -2.0
+    y: -30.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
