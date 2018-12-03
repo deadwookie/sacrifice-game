5991aa2464722c53534347b9a38159a6184ed551
Pavel Zet <zetorama@gmail.com>
2018-12-02 17:42:20 -0800

    improve road and obstacle tiles

---
+++ SacrificeGame/war/helicopter.go
@@ -0,0 +1,40 @@
+embedded_components {
+  id: "base"
+  type: "sprite"
+  data: "tile_set: \"/assets/atlases/helicopter.atlas\"\n"
+  "default_animation: \"Helicopter_Base\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "screw"
+  type: "sprite"
+  data: "tile_set: \"/assets/atlases/helicopter.atlas\"\n"
+  "default_animation: \"Helicopter_Screw_4x\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
